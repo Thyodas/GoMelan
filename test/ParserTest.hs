89b1713e6ecf352c8dbc0ce5872a4495b10c305c
Thyodas <guillaume.hein@epitech.eu>
2023-09-28 14:04:15 +0200

    test(parser): add tests to increase coverage on parser

--- test/ParserTest.hs
+++ test/ParserTest.hs
@@ -4,6 +4,24 @@ import Test.HUnit
 import Parser (parseAnyChar, parseChar, parseOr, parseAnd, parseAndWith,
     parseMany, parseSome, parseInt, parsePair, parseList, Parser, runParser)
 
+testParseChar :: Test
+testParseChar = TestCase $ assertEqual "parseChar" expected result
+    where
+        result = runParser (parseChar 'a') "abcd"
+        expected = Right ('a', "bcd")
+
+testParseCharFail :: Test
+testParseCharFail = TestCase $ assertEqual "parseChar failure" expected result
+    where
+        result = runParser (parseChar 'a') "defg"
+        expected = Left "Expected 'a' but got 'd'."
+
+testParseCharEmpty :: Test
+testParseCharEmpty = TestCase $ assertEqual "parseChar empty" expected result
+    where
+        result = runParser (parseChar 'a') ""
+        expected = Left "Expected 'a' but got empty string."
+
 testParseAnyChar :: Test
 testParseAnyChar = TestCase $ assertEqual "parseAnyChar" expected result
     where
@@ -52,11 +70,23 @@ testParseAndFirstFail = TestCase $ assertEqual "parseAnd first failure" expected
         result = runParser (parseAnd (parseChar 'c') (parseChar 'b')) "bcd"
         expected = Left "Expected 'c' but got 'b'."
 
+testParseAndSecondFail :: Test
+testParseAndSecondFail = TestCase $ assertEqual "parseAnd fail" expected result
+    where
+        result = runParser (parseAnd (parseChar 'a') (parseChar 'b')) "ac"
+        expected = Left "Expected 'b' but got 'c'."
+
 testParseAndWith :: Test
 testParseAndWith = TestCase $ assertEqual "parseAndWith valid" expected result
     where
         result = runParser (parseAndWith (\ x y -> [x,y]) (parseChar 'a') (parseChar 'b')) "abcd"
         expected = Right ("ab", "cd")
+
+testParseAndWithFail :: Test
+testParseAndWithFail = TestCase $ assertEqual "parseAndWith fail" expected result
+    where
+        result = runParser (parseAndWith (\ x y -> [x,y]) (parseChar 'a') (parseChar 'b')) "ac"
+        expected = Left "Expected 'b' but got 'c'."
 
 testParseManyOne :: Test
 testParseManyOne = TestCase $ assertEqual "parseMany one" expected result
@@ -69,6 +99,12 @@ testParseManyTwo = TestCase $ assertEqual "parseMany two" expected result
     where
         result = runParser (parseMany (parseChar ' ')) "foobar   "
         expected = Right ("", "foobar   ")
+
+testParseManyEmpty :: Test
+testParseManyEmpty = TestCase $ assertEqual "parseMany empty" expected result
+    where
+        result = runParser (parseMany (parseChar ' ')) ""
+        expected = Right ("", "")
 
 testParseSomeValid :: Test
 testParseSomeValid = TestCase $ assertEqual "parseSome valid" expected result
@@ -100,11 +136,23 @@ testParseIntNegative = TestCase $ assertEqual "parseInt negative" expected resul
         result = runParser (parseInt :: Parser Int) "-42foobar"
         expected = Right (-42, "foobar")
 
+testParseIntFail :: Test
+testParseIntFail = TestCase $ assertEqual "parseInt fail" expected result
+    where
+        result = runParser (parseInt :: Parser Int) "foobar"
+        expected = Left "Expected one of '0123456789' but got 'f'."
+
 testParsePair :: Test
 testParsePair = TestCase $ assertEqual "parsePair valid" expected result
     where
         result = runParser (parsePair parseInt) "(123 456)foo bar"
         expected = Right ((123, 456), "foo bar")
+
+testParsePairFail :: Test
+testParsePairFail = TestCase $ assertEqual "parsePair fail" expected result
+    where
+        result = runParser (parsePair parseInt) "(123 456 foo bar"
+        expected = Left "Expected ')' but got 'f'."
 
 testParseList :: Test
 testParseList = TestCase $ assertEqual "parseList valid" expected result
@@ -112,8 +160,17 @@ testParseList = TestCase $ assertEqual "parseList valid" expected result
         result = runParser (parseList parseInt) "(  1 2 3 5 7 11 13    17    ) "
         expected = Right ([1,2,3,5,7,11,13,17]," ")
 
+testParseListFail :: Test
+testParseListFail = TestCase $ assertEqual "parseList fail" expected result
+    where
+        result = runParser (parseList parseInt) "(  1 2 3 5 7 11 13    17    "
+        expected = Left "Expected ')' but got empty string."
+
 parserTestList :: Test
 parserTestList = TestList [
+    testParseChar,
+    testParseCharFail,
+    testParseCharEmpty,
     testParseAnyChar,
     testParseAnyCharFailEmpty,
     testParseAnyCharFail,
@@ -122,14 +179,20 @@ parserTestList = TestList [
     testParseOrBothFail,
     testParseAnd,
     testParseAndFirstFail,
+    testParseAndSecondFail,
     testParseAndWith,
+    testParseAndWithFail,
     testParseManyOne,
     testParseManyTwo,
+    testParseManyEmpty,
     testParseSomeValid,
     testParseSomeFail,
     testParseInt,
     testParseIntTwo,
     testParseIntNegative,
+    testParseIntFail,
     testParsePair,
-    testParseList
+    testParsePairFail,
+    testParseList,
+    testParseListFail
     ]
