49885ab2743f3e2595680e113c4532c6b4ab772d
Guillaume HEIN <53176398+Thyodas@users.noreply.github.com>
2023-10-02 18:06:50 +0200

    feat(prompt): add prompt REPL (#14)
    * feat: prompt
    
    * feat(prompt): link main to repl
    
    * feat(prompt): add multiline + CTRL-D handling
    
    ---------
    
    Co-authored-by: lucas <lucas.rossignon@epitech.eu>

--- app/Main.hs
+++ app/Main.hs
@@ -5,29 +5,33 @@ import Execution (runCode)
 import File (readFileEither)
 import System.Environment (getArgs, getProgName)
 import System.Exit ( exitWith, ExitCode (ExitFailure))
-
-handleArgs :: IO (Either String [String])
-handleArgs = do
-    args <- getArgs
-    if length args /= 1 then
-        return $ Left "Usage: ./funEvalExpr <file>"
-    else
-        return $ Right args
+import Prompt (replLoop)
 
 exitOnError :: String -> Either String a -> IO a
 exitOnError progName (Left errorMsg) = putStrLn (progName ++ ": " ++ errorMsg)
             >> exitWith (ExitFailure 84)
 exitOnError _ (Right el) = return el
 
-main :: IO ()
-main = do
-    progName <- getProgName
-    args <- handleArgs
-    [path] <- exitOnError progName args
+fileExecution :: String -> IO ()
+fileExecution path = do
     file <- readFileEither path
     case file of
         Left err -> putStrLn err >> exitWith (ExitFailure 84)
         Right content -> case runCode internalEnv content of
             Left err -> putStrLn err >> exitWith (ExitFailure 84)
-            Right asts -> mapM_ (putStrLn . show) asts
+            Right (_, asts) -> mapM_ (putStrLn . show) asts
 
+replExecution :: IO ()
+replExecution = putStrLn "Welcome to GLaDOS!" >> replLoop internalEnv
+
+startExecution :: [String] -> IO ()
+startExecution [path] = fileExecution path
+startExecution [] = replExecution
+startExecution _ = putStrLn "Usage: ./funEvalExpr [file]"
+    >> exitWith (ExitFailure 84)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    -- [path] <- exitOnError progName args
+    startExecution args
