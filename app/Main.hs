b8777b2144a6ee85694993610b656f8786daecc1
lucas <lucas.rossignon@epitech.eu>
2023-10-02 16:18:49 +0200

    feat: prompt

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,32 @@ import File (readFileEither)
 import System.Environment (getArgs, getProgName)
 import System.Exit ( exitWith, ExitCode (ExitFailure))
 
+import System.IO
+import System.Exit
+
+-- main :: IO ()
+-- main = do
+--     -- putStrLn "Welcom to my Haskell Prompt!"
+--     putStrLn "Type ':q' to quit."
+--     replLoop
+
+replLoop :: IO ()
+replLoop = do
+    putStr "Prelude> "
+    hFlush stdout
+    input <- getLine
+    if input == ":q"
+        then do
+            putStrLn "Exit..."
+            exitSuccess
+        else do
+            processInput input
+            replLoop
+
+processInput :: String -> IO ()
+processInput input = putStrLn $ input -- fonction who manage error
+
+
 handleArgs :: IO (Either String [String])
 handleArgs = do
     args <- getArgs
