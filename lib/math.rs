88441cb8001a7083011d56318560b213ef2522f7
Sauterelle57 <marie.giacomel@epitech.eu>
2023-11-02 17:51:58 +0100

    feat(math): add function power, round, abs, factorial, sum, isPrime,

--- lib/math.rs
+++ lib/math.rs
@@ -1,89 +1,119 @@
-fn power(x: Int) -> Int
+fn power(x: Int, pow:Int) -> Int
 {
-    retrurn x;
+    if (x == 0) {
+        return 0;
+    }
+    if (y < 0) {
+        return (power(x, y + 1) / x);
+    }
+    return (power(x, y - 1) * x);
 }
 
-fn round(x: Int) -> Int
+fn round(x: Float, y: Int) -> Float
 {
-    retrurn x;
+    return x;
 }
 
 fn abs(x: Int) -> Int
 {
-    retrurn x;
+    if (x < 0) {
+        return (x * -1);
+    }
+    return x;
 }
 
-fn facorial(x: Int) -> Int
+fn factorial(x: Int) -> Int
 {
-    retrurn x;
+    if (x < 1) {
+        return x * factorial(x - 1);
+    }
+    return 1;
 }
 
-fn sum(x: Int) -> Int
+fn sum(x: [Int]) -> Int
 {
-    retrurn x;
+    res: Int = 0;
+    for (i: Int = 0; i < x; i = i + 1) {
+        res = res + x[i];
+    }
+    return x;
 }
 
 fn square(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
-fn isPrime(x: Int) -> Int
+fn isPrime(x: Int) -> Bool
 {
-    retrurn x;
+    if (n <= 1) {
+        return 0;
+    }
+    if (n == 2 || n == 3) {
+        return 1;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return 0;
+    }
+    for (int i = 5; i * i <= n; i = i + 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 fn log(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn nlog(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn exp(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn cos(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn sin(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn tan(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn acos(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn asin(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn atan(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn degrees(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
 
 fn radians(x: Int) -> Int
 {
-    retrurn x;
+    return x;
 }
