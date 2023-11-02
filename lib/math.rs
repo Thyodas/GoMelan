cd14e6b163075fe4e54437de01aed41b8cad230f
Sauterelle57 <marie.giacomel@epitech.eu>
2023-11-02 17:31:51 +0100

    feat(math): add template for function in math lib

---
+++ lib/math.rs
@@ -0,0 +1,89 @@
+fn power(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn round(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn abs(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn facorial(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn sum(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn square(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn isPrime(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn log(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn nlog(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn exp(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn cos(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn sin(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn tan(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn acos(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn asin(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn atan(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn degrees(x: Int) -> Int
+{
+    retrurn x;
+}
+
+fn radians(x: Int) -> Int
+{
+    retrurn x;
+}
