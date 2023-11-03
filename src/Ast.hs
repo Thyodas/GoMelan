c2e5e1290dca38ba367b9c74ae53888d5088392c
Thomas Parenteau <thomas.parenteau@epitech.eu>
2023-11-03 10:46:07 +0100

    fix(feat/vm): precedence

--- src/Ast.hs
+++ src/Ast.hs
@@ -240,18 +240,19 @@ gomExprToAGomAssignment _ got = throwEvalError "Expected an Assignment" [got]
 
 precedence :: GomExpr -> Int
 precedence (Operator op) = case op of
-  "+" -> 1
-  "-" -> 1
-  "*" -> 2
-  "/" -> 2
-  "%" -> 2
-  "==" -> 3
-  "!=" -> 3
-  "<=" -> 3
-  ">=" -> 3
-  "<" -> 3
-  ">" -> 3
-  "&&" -> 4
+  "||" -> 1
+  "&&" -> 1
+  "==" -> 2
+  "!=" -> 2
+  "<=" -> 2
+  ">=" -> 2
+  "<" -> 2
+  ">" -> 2
+  "+" -> 3
+  "-" -> 3
+  "*" -> 4
+  "/" -> 4
+  "%" -> 4
   "!" -> 5
   _ -> 0
 precedence _ = 0
