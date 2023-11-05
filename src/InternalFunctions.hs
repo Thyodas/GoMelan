6f714e3c40197cba5d0de425a940537a09479301
Sauterelle57 <marie.giacomel@epitech.eu>
2023-11-05 19:05:21 +0100

    feat(internalFunctions): add divers conversion functions

--- src/InternalFunctions.hs
+++ src/InternalFunctions.hs
@@ -13,7 +13,11 @@ astInternalEnv :: Env
 astInternalEnv = [
         lenFuncType,
         intToFloatFuncType,
-        floatToIntFuncType
+        floatToIntFuncType,
+        stringToIntFuncType,
+        intToStringFuncType,
+        intToBoolFuncType,
+        floatToStringFuncType
     ]
 
 lenFuncType :: EnvEntry
@@ -40,3 +44,34 @@ floatToIntFuncType = (name,
         args = [AGomTypedIdentifier "float" (AGomType "Float")]
         returnType = AGomType "Int"
 
+stringToIntFuncType :: EnvEntry
+stringToIntFuncType = (name,
+    AGomInternalFunction name (AGomParameterList args) returnType)
+    where
+        name = "stringToInt"
+        args = [AGomTypedIdentifier "float" (AGomTypeList [AGomType "Char"])]
+        returnType = AGomType "Int"
+
+intToStringFuncType :: EnvEntry
+intToStringFuncType = (name,
+    AGomInternalFunction name (AGomParameterList args) returnType)
+    where
+        name = "intToString"
+        args = [AGomTypedIdentifier "int" (AGomType "Int")]
+        returnType = (AGomTypeList [AGomType "Char"])
+
+intToBoolFuncType :: EnvEntry
+intToBoolFuncType = (name,
+    AGomInternalFunction name (AGomParameterList args) returnType)
+    where
+        name = "intToBool"
+        args = [AGomTypedIdentifier "int" (AGomType "Int")]
+        returnType = AGomType "Bool"
+
+floatToStringFuncType :: EnvEntry
+floatToStringFuncType = (name,
+    AGomInternalFunction name (AGomParameterList args) returnType)
+    where
+        name = "floatToString"
+        args = [AGomTypedIdentifier "float" (AGomType "Float")]
+        returnType = (AGomTypeList [AGomType "Char"])
--- src/VirtualMachine/InternalFunctions.hs
+++ src/VirtualMachine/InternalFunctions.hs
@@ -9,8 +9,9 @@ module VirtualMachine.InternalFunctions (vmInternalEnv) where
 
 import VirtualMachine.Vm (Args, Val(..), VmEnv, InternalFunction(..))
 import Data.Either()
-
+import Data.Char (isDigit)
 import Data.Binary()
+import Text.Read (readMaybe)
 import qualified Data.ByteString.Lazy as BS
 
 internalEntry :: String -> (Args -> Either String Val) -> (String, Val)
@@ -20,7 +21,11 @@ vmInternalEnv :: VmEnv
 vmInternalEnv = [
         internalEntry "len" lenOfList,
         internalEntry "intToFloat" intToFloat,
-        internalEntry "floatToInt" floatToInt
+        internalEntry "floatToInt" floatToInt,
+        internalEntry "stringToInt" stringToInt,
+        internalEntry "intToString" intToString,
+        internalEntry "intToBool" intToBool,
+        internalEntry "floatToString" floatToString
     ]
 
 lenOfList :: Args -> Either String Val
@@ -29,11 +34,44 @@ lenOfList [VList list] = Right $ VNum $ length list
 lenOfList _ = Left "lenOfList: invalid argument"
 
 intToFloat :: Args -> Either String Val
-intToFloat [] = Left "intToFloat: invalid number of argumentss"
+intToFloat [] = Left "intToFloat: invalid number of arguments"
 intToFloat [VNum x] = Right $ VFloatNum $ fromIntegral x
 intToFloat _ = Left "intToFloat: invalid argument type expected Int"
 
 floatToInt :: Args -> Either String Val
-floatToInt [] = Left "floatToInt: invalid number of argumentss"
+floatToInt [] = Left "floatToInt: invalid number of arguments"
 floatToInt [VFloatNum x] = Right $ VNum $ round x
 floatToInt _ = Left "floatToInt: invalid argument type expected Int"
+
+stringToInt :: Args -> Either String Val
+stringToInt [] = Left "stringToInt: invalid number of arguments"
+stringToInt [str@(VList (VChar _:_))] = case findInt (concatCharList str) of
+    Just num -> Right $ VNum num
+    Nothing -> Left $ "stringToInt: no integer found in '" ++ show str ++ "'"
+    where
+        concatCharList :: Val -> String
+        concatCharList (VList charList) = concatMap extractChar charList
+        concatCharList _ = ""
+
+        extractChar :: Val -> String
+        extractChar (VChar c) = [c]
+        extractChar _ = ""
+
+        findInt :: String -> Maybe Int
+        findInt str' = readMaybe str'
+stringToInt _ = Left "stringToInt: invalid argument type expected String"
+
+intToString :: Args -> Either String Val
+intToString [] = Left "intToString: invalid number of arguments"
+intToString [VNum x] = Right $ VList $ map VChar $ show x
+intToString _ = Left "intToString: invalid argument type expected Int"
+
+intToBool :: Args -> Either String Val
+intToBool [] = Left "intToBool: invalid number of arguments"
+intToBool [VNum x] = Right $ VBool $ x /= 0
+intToBool _ = Left "intToBool: invalid argument type expected Int"
+
+floatToString :: Args -> Either String Val
+floatToString [] = Left "floatToString: invalid number of arguments"
+floatToString [VFloatNum x] = Right $ VList $ map VChar $ show x
+floatToString _ = Left "floatToString: invalid argument type expected Float"
